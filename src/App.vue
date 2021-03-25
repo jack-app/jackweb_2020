f309add73ffbbc93e1b978c94d60fec27080f88b
mina1112 <mina.kato8885@gmail.com>
2021-03-25 18:58:11 +0900

    Co-authored-by: yunine16 <yunine16@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,165 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+              color="tranceparent"
+              src="@/assets/triangle-mosaic.png"
+              hide-on-scroll>
+      <v-toolbar-title>jack</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
     </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <div class="content">
+          <div class="title">団体理念</div>
+          <p>やりたいことをやろう</p>
+        </div>
+        <div class="content">
+          <div class="title">団体情報</div>
+          <div class="table-wrapper">
+            <info-table />
+          </div>
+        </div>
+        <div class="content">
+          <div class="title">jackでできること</div>
+        </div>
+        <div class="content products">
+          <div class="products__title title">プロダクト一覧</div>
+          <div class="products__lists">
+            <!-- <Products /> -->
+          </div>
+        </div>
+        <div class="content calendar">
+          <h2 class="title">活動予定</h2>
+          <!-- <Calendar /> -->
+        </div>
+        <div class="content">
+          <div class="title">よくある質問</div>
+          <div class="questions-wrapper">
+            <question-item />
+          </div>
+        </div>
+        <div class="content">
+          <div class="title">お問い合わせ</div>
+          <p>お問い合わせを入れる</p>
+        </div>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import InfoTable from './components/InfoTable.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    InfoTable
   },
 
-  data: () => ({
-    //
-  }),
+  // data: () => ({
+  // }),
 };
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #4c4c4c;
+  background-color: #fff;
+  background-image: url("/triangle-mosaic.png");
+  background-repeat: repeat;
+}
+
+.content {
+  margin-bottom: 5vh;
+}
+
+.title {
+  position: relative;
+  margin-bottom: 20px;
+}
+
+.title:before{
+  content: '';
+  position: absolute;
+  bottom: -5px;
+  display: inline-block;
+  width: 45px;
+  height: 3px;
+  left: 50%;
+  -moz-transform: translateX(-50%);
+  -webkit-transform: translateX(-50%);
+  -ms-transform: translateX(-50%);
+  transform: translateX(-50%);
+  background-color: #F29F05;
+  border-radius: 2px;
+}
+
+.table-wrapper {
+  margin-bottom: 20px;
+}
+
+/*吹き出し*/
+.balloon_l,
+.balloon_r{
+  margin: 30px 0;
+  display:flex;
+  justify-content: flex-start;
+  align-items: flex-start; /*縦位置を上揃え*/
+/*   align-items: center; */ /*縦位置を真ん中揃え*/
+}
+.balloon_r{
+  justify-content:flex-end;
+}
+.faceicon img{
+  width: 80px; /*任意のサイズ*/
+  height: auto;
+}
+.balloon_r .faceicon{
+  margin-left:25px;
+}
+.balloon_l .faceicon{
+  margin-right:25px;
+}
+.balloon_r .faceicon{
+  order:2 !important;
+}
+.says {
+  max-width:500px; /*最大幅は任意*/
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  padding: 17px 13px 15px 18px;
+  border-radius: 12px;
+  background: #FFCC80;/*色は任意*/
+  box-sizing:border-box;
+  margin:20px !important;
+  line-height:1.5;
+/*   align-items: center; */
+}
+.says p{
+  margin:8px 0 0 !important;
+}
+.says p:first-child{
+  margin-top:0 !important;
+}
+.says:after {
+  content: "";
+  position: absolute;
+  border: 10px solid transparent;
+/*   margin-top:-3px;  */
+}
+.balloon_l .says:after {
+  left: -26px;
+  border-right: 22px solid #FFCC80;
+}
+.balloon_r .says:after {
+  right: -26px;
+  border-left: 22px solid #FFCC80;
+}
+</style>
---
+++ src/components/InfoTable.vue
@@ -0,0 +1,82 @@
+<template>
+    <table class="table table-2col">
+        <tbody>
+        <tr>
+            <th>サークル・団体名</th>
+            <td>jack</td>
+        </tr>
+        <tr>
+            <th>人数</th>
+            <td>?人</td>
+        </tr>
+        <tr>
+            <th>活動場所</th>
+            <td>名古屋大学</td>
+        </tr>
+        <tr>
+            <th>活動内容</th>
+            <td>
+            <ul>
+                <li>Android・iOSアプリケーション製作</li>
+                <li>パソコン用ソフトウェア製作</li>
+                <li>サービス・ゲーム企画の構想及び運営</li>
+                <li>Webコンテンツ製作・提供</li>
+                <li>各種ハッカソン・ITイベントへの参加等</li>
+            </ul>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<style scoped>
+table{
+  width: 100%;
+  border-collapse: collapse;
+  color: #4c4c4c;
+}
+
+table tr{
+  border-bottom: solid 2px white;
+}
+
+table tr:last-child{
+  border-bottom: none;
+}
+
+table th{
+  position: relative;
+  text-align: left;
+  width: 30%;
+  background-color: #FFB300;
+  color: white;
+  text-align: center;
+  padding: 10px 0;
+}
+
+table th:after{
+  display: block;
+  content: "";
+  width: 0px;
+  height: 0px;
+  position: absolute;
+  top:calc(50% - 10px);
+  right:-10px;
+  border-left: 10px solid #FFB300;
+  border-top: 10px solid transparent;
+  border-bottom: 10px solid transparent;
+}
+
+table td{
+  text-align: left;
+  width: 70%;
+  text-align: center;
+  background-color: #eee;
+  padding: 10px 0;
+}
+
+ul{
+  list-style: none;
+  padding-left: 0;
+}
+</style>

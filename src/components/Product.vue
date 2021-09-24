2149da4572a5dd607a866d76d1775bc54a8856e8
Sayaki-M <38837489+Sayaki-M@users.noreply.github.com>
2021-09-24 19:06:07 +0900

    add product component

---
+++ src/components/Product.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>お家bar</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
     <p>作品一覧</p>
+    <div>
+      <Product v-for = "product in products"
+        :key = "product.key"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Product from './Product.vue'
 
+export default{
+  name: 'Products',
+  components: {
+    Product
+  },
+  data: () => ({
+    products: []
+  }),
+  created (){
+    for(var i=0;i<5;i++){
+      this.products.push({key:i});
+    }
+  }
+}
 </script>
 
 <style scoped>

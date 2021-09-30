8f9ed8482815e3527998c9d521e29c93b8919f9d
Sayaki-M <38837489+Sayaki-M@users.noreply.github.com>
2021-09-30 19:55:44 +0900

    add お問い合わせに埋め込みフォームを追加

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p>お問い合わせ</p>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdkvLbyvnp6rSOxCweb_tW3CPCF8rG-s6yJ8hV6QmHVp9mNxA/viewform?embedded=true">読み込んでいます…</iframe>
   </div>
 </template>
 

f331a50691fc3125c6073e18f96095c76dc0905c
Sayaki-M <38837489+Sayaki-M@users.noreply.github.com>
2021-10-12 01:10:53 +0900

    add ヘッダーの入部希望者ボタンを消すようにした

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,16 @@
     <router-link to="/activity">活動内容</router-link>
     <router-link to="/results">実績</router-link>
     <router-link to="/contact">お問い合わせ</router-link>
-    <router-link to="/">入部希望者はこちら</router-link>
+    <router-link
+      to="/"
+      v-if="
+        this.$route.path.match('/products') ||
+          this.$route.path.match('/activity') ||
+          this.$route.path.match('/results') ||
+          this.$route.path.match('/contact')
+      "
+      >入部希望者はこちら</router-link
+    >
   </header>
 </template>
 

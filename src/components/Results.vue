edca993037ec97c53f672ee6a0d1d972dd85c78a
Sayaki-M <38837489+Sayaki-M@users.noreply.github.com>
2021-10-12 00:26:54 +0900

    add 実績一覧にサンプル実績を追加

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,16 +1,39 @@
 <template>
   <div>
     <p>実績紹介</p>
-    <v-card>
-      <v-card-subtitle class="text-left">RESULTDATE</v-card-subtitle>
-      <v-card-title>RESULTNAME</v-card-title>
+    <v-card v-for="result in results" :key="result.name">
+      <v-card-subtitle class="text-left">{{ result.date }}</v-card-subtitle>
+      <v-card-title>{{ result.name }}</v-card-title>
       <v-card-actions class="justify-end">
-        <v-btn>read more</v-btn>
+        <a v-if="result.link" :href="result.link"><v-btn>read more</v-btn></a>
       </v-card-actions>
     </v-card>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "Result",
+  data: () => ({
+    results: [
+      {
+        name: "JPHACKS2020 Best Audience Award他「コミックSE」",
+        date: "November 28, 2020",
+        link: "https://jphacks.com/2020/result/innovator/"
+      },
+      {
+        name: "通知で覚える英単語 リリース",
+        date: "October 24, 2020",
+        link: "https://apps.apple.com/jp/app/通知で覚える英単語/id1537063008"
+      },
+      {
+        name: "サンプル実績",
+        date: "September 28, 2020",
+        link: "https://google.com"
+      }
+    ]
+  })
+};
+</script>
 
 <style scoped></style>

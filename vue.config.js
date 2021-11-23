a31c9c8fcf841913ee91e6b41823ac71d88185c9
Sayaki-M <38837489+Sayaki-M@users.noreply.github.com>
2021-11-23 14:00:22 +0900

    api整備

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  pages:{
+  pages: {
     index: {
       entry: "src/main.js",
       title: "アプリ開発団体jack"
     }
   },
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  transpileDependencies: ["vuetify"],
+  devServer: {
+    // localhostでvueからexpressにAPIリクエストを送信する為の設定
+    proxy: {
+      "^/api": {
+        target: "http://localhost:3000",
+        secure: false
+      }
+    }
+  }
+};

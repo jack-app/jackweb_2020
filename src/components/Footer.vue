39b4ecfea39526b6832d9ed904ec53519d3d2896
Sayaki-M <38837489+Sayaki-M@users.noreply.github.com>
2021-12-04 17:38:40 +0900

    add フッターにcssつけた

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,46 @@
 <template>
   <footer>
-    <div>
-      <!--Vuetifyにあるメールのアイコン-->
-
-      <a href="mailto:jack.app.tmy@gmail.com"
-        ><v-icon large>mdi-email</v-icon>jack.app.tmy@gmail.com</a
-      >
-
-      <a href="https://twitter.com/jackapp_tmy/"
-        ><img src="@/assets/twitter.png" />@jackapp_tmy</a
-      >
-    </div>
-
-    <p>＠jack 2021</p>
+    <v-container>
+      <v-row>
+        <v-col cols="4">
+          <v-btn
+            class="contact__icon"
+            href="mailto:jack.app.tmy@gmail.com"
+            icon
+          >
+            <v-icon large>mdi-email</v-icon>
+          </v-btn>
+          <v-btn
+            class="contact__icon"
+            href="https://twitter.com/jackapp_tmy/"
+            icon
+          >
+            <v-icon large>mdi-twitter</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <p class="copylight">©jack 2021</p>
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-container>
   </footer>
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+footer {
+  background-color: #fc913a;
+}
+
+.contact__icon {
+  margin: auto 2px;
+}
+
+.copylight {
+  display: inline-block;
+  vertical-align: middle;
+  padding-top: 3px;
+}
+</style>

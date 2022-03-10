8c0e0efd33edc529fa07236d41230694a0b83b1a
mina1112 <mina.kato8885@gmail.com>
2022-03-10 19:08:30 +0900

    welcomeページの編集

--- src/views/welcome/Welcome.vue
+++ src/views/welcome/Welcome.vue
@@ -1,7 +1,15 @@
 <template>
   <v-container class="welcome">
+    <WelcomeActivity />
   </v-container>
 </template>
 
 <script>
+import WelcomeActivity from "@/components/Welcome-Activity.vue";
+
+export default {
+  components: {
+    WelcomeActivity,
+  },
+};
 </script>
